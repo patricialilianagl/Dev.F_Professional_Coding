5b12dab82b37457f04537c4b741f1bb8bfcc4c16
Patricia Liliana <liliana.loman.13@gmail.com>
2022-06-08 21:26:49 -0500

    .

---
+++ JS/0_Intro_JS/main.js
@@ -0,0 +1,75 @@
+
+// alert ("Hola")
+
+
+// document.write("Hola")
+
+// Imprimir en console
+// console.log("Hola desde consola")
+// console.log(5+3)
+// console.log(4 > 3)
+
+
+// Imprimir en nuestra página web
+// document.write("Hola en nuestra página web desde JavaScript");
+
+// Imprimir mediante una alerta emergente
+// alert("Hola desde alert")
+
+// variables
+// var NombreDeUsuario = prompt("Ingrese su nombre")
+
+// Pedir datos al ususario
+// prompt("Ingrese su nombre")
+
+
+
+// document.write("Hola " + nombreDeUsuario)
+// console.log("Hola, ¿Cómo estás? " + nombreDeUsuario)
+
+// Variable de tipo string 
+var nombreDeUsuario = "Patricia"
+
+// Variable de tipo Number
+var tipoNumber = 2;
+var tipoDouble = 2.2;
+
+// tipo de dato Boolean
+var tipoBoolean = false;
+var tipoBooleanTrue = true;
+
+var tipoNull = null;
+
+var tipoUndefined;
+
+//console.log(tipoUndefined)
+
+var tipoObjeto = {
+    nombre: "Patricia",
+    apellido: "García",
+    edad: 27
+}
+
+// {}  llaves
+// [] corchete
+// () parantesis
+
+// console.log(tipoObjeto)
+
+var numero1 = prompt("Ingrese el primer numero a sumar?")
+var numero2 = prompt("Ingrese el segundo numero a sumar?")
+var suma = numero1 + numero2;
+document.write("Suma de " + numero1 +" + " + numero2 +" es: " + suma)
+
+var suma = 4 + 4;
+console.log("Suma de 4 + 4 es:", suma)
+var resta = 3 - 2;
+console.log("Resta de 3 - 2 es :", resta)
+var multiplicacion = 3 * 5;
+console.log("Multiplicacion de 3 * 5 es:", multiplicacion)
+var division = 4 / 2
+console.log("Division de 4 / 2 es:", division)
+var modulo = 4 % 2
+console.log("Modulo de 4 / 2 es:", modulo)
+var exponencial = 2**2
+console.log("Exponencial de 2**2 es:", exponencial)
